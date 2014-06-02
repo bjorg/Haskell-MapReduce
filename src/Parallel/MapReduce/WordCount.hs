08575aa009c3a8269d6d511c2154f8ed3e047ebf
Steve Bjorg <steve.bjorg@gmail.com>
2014-06-01 17:04:41 -0700

    formatting clean up

--- src/Parallel/MapReduce/WordCount.hs
+++ src/Parallel/MapReduce/WordCount.hs
@@ -1,8 +1,8 @@
 -- | Module that uses "Process.MapReduce.Multicore" to implement
 --   the standard Mapreduce wordcount algorithm.
 module Parallel.MapReduce.WordCount (
-        mapReduce
-        ) where
+    mapReduce
+) where
 
 --import Parallel.MapReduce.Multicore
 import Parallel.MapReduce.Simple (run,distribute,lift,(>>=))
@@ -10,18 +10,16 @@ import Prelude hiding (return,(>>=))
 
 -- | Perform MapReduce on a list of words, returning word / count pairs
 mapReduce :: Int                -- ^ The number of mappers to use on the first stage
-        -> [String]             -- ^ The list of words to count
-        -> [(String,Int)]       -- ^ The list of word / count pairs
-mapReduce n state = run mr state
-        where
-        mr = distribute n >>= lift mapper >>= lift reducer
+    -> [String]             -- ^ The list of words to count
+    -> [(String,Int)]       -- ^ The list of word / count pairs
+mapReduce n state = run (distribute n >>= lift mapper >>= lift reducer) state
 
 -- transformers
 
 mapper :: [String] -> [(String,String)]
 mapper [] = []
 mapper (x:xs) = parse x ++ mapper xs
-        where
+    where
         parse x' = map (\w -> (w,w)) $ words x'
 
 reducer :: [String] -> [(String,Int)]
